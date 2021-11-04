b828dfe4f8d3f389fe5354c2e8dff943b9a1bafd
Thomas <65050898+tvonment@users.noreply.github.com>
2021-11-04 14:34:35 +0100

    update mailtext

--- WebFormHelfende/index.ts
+++ WebFormHelfende/index.ts
@@ -69,10 +69,10 @@ async function sendMail(token: string, body: any) {
     },
     data: {
       "message": {
-        "subject": "DANKE fürs Helfen",
+        "subject": "Bestätigung Helfende Kulti22",
         "body": {
           "contentType": "Text",
-          "content": "Hallo " + body.vorname + "\n\nVielen Dank für deine Teilnehme am Kulti22! Wir werden dir neue Informationen schicken, sobald es soweit ist.\n\nFeurige Grüsse\nDas Kulti22 Team"
+          "content": "Hallo " + body.vorname + "\n\n<strong>Wir freuen uns sehr, dass du uns dabei unterstützt, das Kulti 22 unvergesslich zu machen. Schon jetzt ganz herzlichen Dank dafür!</strong>\nDie detaillierten Informationen zu deinem Helfendeneinsatz wirst du im Sommer 2022 erhalten.\nBei allfälligen Fragen wende dich bitte an: simona@kulti22.ch\n\nFeurige Grüsse\nDas Kulti22 Team"
         },
         "toRecipients": [
           {
