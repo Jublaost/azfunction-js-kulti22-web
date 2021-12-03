dafb7d59ffb69b31629b64cf69608d884efaa520
Thomas <65050898+tvonment@users.noreply.github.com>
2021-12-03 13:30:02 +0000

    add reCAPTCHA

--- WebFormKontakt/index.ts
+++ WebFormKontakt/index.ts
@@ -12,27 +12,55 @@ const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
 const MS_GRAPH_ENDPOINT_SENDMAIL = 'https://graph.microsoft.com/v1.0/users/info@kulti22.ch/sendMail';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    context.log("Body: ", req.body)
+  context.log('HTTP trigger function processed a request.');
+  context.log("Body: ", req.body)
 
+  let validation = await validateRECAP(req.body["g-recaptcha-response"]);
+  context.log(validation);
+
+  if (validation) {
     // Set Default Header for Axios Requests
     axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
     let token = await getToken();
     let response = await sendMail(token, req.body);
-    
+
     await sendToTeams(req.body);
 
     context.log(response)
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: req.body
+      // status: 200, /* Defaults to 200 */
+      body: req.body
     };
-
+  } else {
+    context.log("validation failed");
+    context.res = {
+      status: 500
+    }
+  }
 };
 export default httpTrigger;
 
-async function sendToTeams(body:any) {
+async function validateRECAP(token: string) {
+  let config: AxiosRequestConfig = {
+    method: 'post',
+    url: "https://www.google.com/recaptcha/api/siteverify",
+    params: {
+      secret: "6Le6UXYdAAAAAMugTHAfZoozHLgn7AV5XnIa_7Gc",
+      response: token
+    }
+  }
+
+  return await axios(config)
+    .then(response => {
+      return response.data.success;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+async function sendToTeams(body: any) {
   let config: AxiosRequestConfig = {
     method: 'post',
     url: TEAMS_WEBHOOK,
@@ -40,115 +68,115 @@ async function sendToTeams(body:any) {
       'ContentType': 'Application/Json'
     },
     data: {
-      "type": "message",
-      "attachments": [
-          {
-              "contentType": "application/vnd.microsoft.card.adaptive",
-              "content": {
-                  "type": "AdaptiveCard",
-                  "body": [
+      "type": "message",
+      "attachments": [
+        {
+          "contentType": "application/vnd.microsoft.card.adaptive",
+          "content": {
+            "type": "AdaptiveCard",
+            "body": [
+              {
+                "type": "ColumnSet",
+                "columns": [
+                  {
+                    "type": "Column",
+                    "width": "stretch",
+                    "items": [
                       {
-                          "type": "ColumnSet",
-                          "columns": [
-                              {
-                                  "type": "Column",
-                                  "width": "stretch",
-                                  "items": [
-                                      {
-                                          "type": "TextBlock",
-                                          "text": body.name,
-                                          "wrap": true,
-                                          "size": "Medium",
-                                          "weight": "Bolder"
-                                      }
-                                  ]
-                              },
-                              {
-                                  "type": "Column",
-                                  "width": "stretch",
-                                  "items": [
-                                      {
-                                          "type": "TextBlock",
-                                          "text": body.email,
-                                          "wrap": true,
-                                          "weight": "Lighter"
-                                      }
-                                  ]
-                              }
-                          ]
-                      },
-                      {
-                          "type": "TextBlock",
-                          "text": body.message,
-                          "wrap": true
+                        "type": "TextBlock",
+                        "text": body.name,
+                        "wrap": true,
+                        "size": "Medium",
+                        "weight": "Bolder"
                       }
-                  ],
-                  "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                  "version": "1.3"
+                    ]
+                  },
+                  {
+                    "type": "Column",
+                    "width": "stretch",
+                    "items": [
+                      {
+                        "type": "TextBlock",
+                        "text": body.email,
+                        "wrap": true,
+                        "weight": "Lighter"
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                "type": "TextBlock",
+                "text": body.message,
+                "wrap": true
               }
+            ],
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "version": "1.3"
           }
-      ] 
-    } 
+        }
+      ]
+    }
   }
 
   await axios(config);
 }
 
-async function sendMail(token:string, body:any) {
+async function sendMail(token: string, body: any) {
   let config: AxiosRequestConfig = {
-      method: 'post',
-      url: MS_GRAPH_ENDPOINT_SENDMAIL,
-      headers: {
-        'Authorization': 'Bearer ' + token //the token is a variable which holds the token
+    method: 'post',
+    url: MS_GRAPH_ENDPOINT_SENDMAIL,
+    headers: {
+      'Authorization': 'Bearer ' + token //the token is a variable which holds the token
+    },
+    data: {
+      "message": {
+        "subject": "Kontaktaufnahme",
+        "body": {
+          "contentType": "Text",
+          "content": "Hallo " + body.name + "\n\nVielen Dank für deine Kontaktaufnahme! Wir werden uns so schnell wie möglich wieder bei dir melden.\n\nFeurige Grüsse\nDas Kulti22 Team"
+        },
+        "toRecipients": [
+          {
+            "emailAddress": {
+              "address": body.email
+            }
+          }
+        ]
       },
-      data: {
-          "message": {
-            "subject": "Kontaktaufnahme",
-            "body": {
-              "contentType": "Text",
-              "content": "Hallo " + body.name + "\n\nVielen Dank für deine Kontaktaufnahme! Wir werden uns so schnell wie möglich wieder bei dir melden.\n\nFeurige Grüsse\nDas Kulti22 Team"
-            },
-            "toRecipients": [
-              {
-                "emailAddress": {
-                  "address": body.email
-                }
-              }
-            ]
-          },
-          "saveToSentItems": "true"
-        }
+      "saveToSentItems": "true"
+    }
   }
-  
+
   return await axios(config)
-      .then(response => {
-          console.log(response.data);
-          return response.data.value;
-      })
-      .catch(error => {
-          console.log(error);
-      });
+    .then(response => {
+      console.log(response.data);
+      return response.data.value;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 
 /**
  * Get Token for MS Graph
  */
- async function getToken(): Promise<string> {
-    const postData = {
-        client_id: APP_ID,
-        scope: MS_GRAPH_SCOPE,
-        client_secret: APP_SECRET,
-        grant_type: 'client_credentials'
-    };
+async function getToken(): Promise<string> {
+  const postData = {
+    client_id: APP_ID,
+    scope: MS_GRAPH_SCOPE,
+    client_secret: APP_SECRET,
+    grant_type: 'client_credentials'
+  };
 
-    return await axios
-        .post(TOKEN_ENDPOINT, qs.stringify(postData))
-        .then(response => {
-            // console.log(response.data);
-            return response.data.access_token;
-        })
-        .catch(error => {
-            console.log(error);
-        });
+  return await axios
+    .post(TOKEN_ENDPOINT, qs.stringify(postData))
+    .then(response => {
+      // console.log(response.data);
+      return response.data.access_token;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
