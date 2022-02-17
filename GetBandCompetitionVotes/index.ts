dc23a2d3ff4e516904a8d7e9054e7936b5b546a4
Thomas <65050898+tvonment@users.noreply.github.com>
2022-02-17 21:49:34 +0000

    add get function

--- GetBandCompetitionVotes/index.ts
+++ GetBandCompetitionVotes/index.ts
@@ -3,11 +3,20 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
+    let votes: Vote[] = context.bindings.votingEntities;
+
+    const result = votes.reduce((counter, vote) => (counter[vote.Act] = (counter[vote.Act] || 0) + 1, counter), {});
+
+    console.log(result);
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: context.bindings.votingEntities
+        body: result
     };
-
 };
 
 export default httpTrigger;
+
+class Vote {
+    Act: string;
+}
