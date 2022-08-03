8e4e03f2de93b3dda8e776dccf81f8ec4cedfaf9
Thomas <65050898+tvonment@users.noreply.github.com>
2022-08-03 21:38:58 +0000

    add get functions

--- GetGameInfos/index.ts
+++ GetGameInfos/index.ts
@@ -1,15 +1,21 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, gamesIn): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    context.log(gamesIn)
+
+    let resp = {
+        slot01field01: gamesIn.filter(val => val.field === 'Feld01;13:00-14:00').length,
+        slot01field02: gamesIn.filter(val => val.field === 'Feld02;13:00-14:00').length,
+        slot02field01: gamesIn.filter(val => val.field === 'Feld01;14:00-15:00').length,
+        slot02field03: gamesIn.filter(val => val.field === 'Feld02;14:00-15:00').length,
+        slot03field01: gamesIn.filter(val => val.field === 'Feld01;15:00-16:00').length,
+        slot03field02: gamesIn.filter(val => val.field === 'Feld02;15:00-16:00').length
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: resp
     };
 
 };
