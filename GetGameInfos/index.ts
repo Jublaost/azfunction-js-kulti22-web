0a09470e4e474a22b0b8647bb0301345f6adcb6f
Thomas <65050898+tvonment@users.noreply.github.com>
2022-08-18 16:06:09 +0000

    add kick after 1 hout

--- GetGameInfos/index.ts
+++ GetGameInfos/index.ts
@@ -1,8 +1,19 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { CosmosClient } from "@azure/cosmos";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, gamesIn): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     context.log(gamesIn)
+
+    for (let i of gamesIn.filter(x => !x.approved)) {
+        let overTimeLimit: boolean = new Date().getTime() / 1000 > i._ts + 3600
+        if (overTimeLimit) {
+            context.log("Time Limit reached: ", i.name, overTimeLimit);
+            const client = new CosmosClient(process.env.Jublaost_COSMOSDB);
+            const container = client.database("kulti22").container("spikeball")
+            const { resource } = await container.item(i.id, i.id).delete()
+        }
+    }
 
     let resp = {
         slot01field01: gamesIn.filter(val => val.field === 'Feld01;13:00-14:00').length,
